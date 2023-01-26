9dbf443b801f56a48a1c22998a0c99ea7069dc1e
Edward Hernandez <edwardgta4@hotmail.com>
2023-01-26 10:17:56 -0500

    some fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 const Header = () => {
+
+    const [show,setShow]=useState(false);
+
+    const handleToggle = ()=>{
+        setShow(!show);     
+    }
+
   return (
+    <>
     <header>
-        <div className='logo'>_Edward_Hernandez</div>
-        <div className='hamburger'>
+        
+        <div className='logo' >_Edward_Hernandez</div>
+            <div className='hamburger' onClick={handleToggle}>
             <div className='line'></div>
             <div className='line'></div>
             <div className='line'></div>
         </div>
-        <nav className='nav-bar'>
+        <nav className={show ? 'mobile' : 'nav-bar'}>
             <ul>
                 <li>
                     <a href="" className='active'>Home</a>
@@ -22,8 +32,10 @@ const Header = () => {
                 </li>
             </ul>
         </nav>
-
+    
     </header>
+    </>
+    
   )
 }
 
