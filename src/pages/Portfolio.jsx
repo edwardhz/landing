3554c82f72f306f3e761ca45836bf2acdbf58b21
Edward Hernandez <edwardgta4@hotmail.com>
2023-02-02 17:45:29 -0500

    Portfolio2

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -26,7 +26,14 @@ const Portfolio = () => {
               <SplideSlide>
                 <Card>
                   <Link to={'recipe/'}>
-                    <p>Projecto 1</p>
+                    <h4>Shop</h4>
+                    <ul>
+                      <li>React</li>
+                      <li>Tailwind</li>
+                      <li>Webpack</li>
+                      <li>HTML</li>
+                      <li>CSS</li>
+                    </ul>
                     <img src='https://cdn.pixabay.com/photo/2023/01/26/18/09/zebra-7746719_960_720.jpg' alt='Project 1' />
                     <Gradient/>
                   </Link>
@@ -67,6 +74,7 @@ const Wrapper = styled.div`
   }
 `
 const Card = styled.div`
+  margin-top: 5px;
   min-height: 30rem;
   border-radius: 2rem;
   overflow: hidden;
@@ -80,6 +88,52 @@ const Card = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+  h4{
+    z-index: 10;
+    position: absolute;
+    bottom: 0%;
+    left: 50%;
+    transform: translate(-50%,0%);
+    color: white;
+    width: 100%;
+    text-align:center;
+    font-weight: 600;
+    font-size: 2.5rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+  }
+  ul{
+    z-index: 10;
+    position: absolute;
+    bottom: 50%;
+    left: 10%;
+    transform: translate(-50%,0%);
+    color: white;
+    text-align:center;
+    font-weight: 600;
+    font-size: 1.2rem;
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    @media (max-width:425px) {
+      left: 50%;
+    }
+
+    li{
+      display: inline-block;
+      padding: 0.5rem 0.5rem;
+      color: #f0ead6;
+      background-color: rgba(112, 160, 175);
+      border-radius: 10%;
+      box-shadow: 3px 3px 0px 0px #f0ead6;
+    }
+    
   }
 `
 const Gradient = styled.div`
